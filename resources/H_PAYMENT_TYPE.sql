05a5a49052679f93f3c3347f8a11ba818d87f5be
Mathias <mathias.puyfages@gmail.com>
2025-01-09 23:27:23 +0100

    fix: fix payment type sql file

--- resources/H_PAYMENT_TYPE.sql
+++ resources/H_PAYMENT_TYPE.sql
@@ -1,6 +1,6 @@
 CREATE TABLE payment_type (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,      
+    name VARCHAR(100) NOT NULL,
     group_id INT DEFAULT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    FOREIGN KEY (group_id) REFERENCES `group`(id)
 );
