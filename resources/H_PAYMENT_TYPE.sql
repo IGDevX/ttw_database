54b40c41e0433de91732f6b17725bf044e9bacf0
Mathias <mathias.puyfages@gmail.com>
2025-01-09 23:21:48 +0100

    feat: ajout de la table payment_type

---
+++ resources/H_PAYMENT_TYPE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE payment_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,      
+    group_id INT DEFAULT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+);
