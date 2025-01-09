815eaec794f30a663f29d204bbd13ad384416312
Mathias <mathias.puyfages@gmail.com>
2025-01-09 23:30:58 +0100

    feat: ajout de données insérer dans la table payment type.

--- resources/H_PAYMENT_TYPE.sql
+++ resources/H_PAYMENT_TYPE.sql
@@ -4,3 +4,15 @@ CREATE TABLE payment_type (
     group_id INT DEFAULT NULL,
     FOREIGN KEY (group_id) REFERENCES `group`(id)
 );
+
+-- Insertion des moyens de paiement
+INSERT INTO payment_type (name, group_id) VALUES
+-- Moyens de paiement communs sans groupe
+('Cash', NULL),
+('Credit Card', NULL),
+('Bank Transfer', NULL),
+
+-- Moyens de paiement spécifiques liés à des groupes
+('Paylib', 1),
+('PayPal', 1),
+('Stripe', 2),
