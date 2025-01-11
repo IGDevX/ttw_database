0a4415a9717e4b289f7a83b234c20c61b292170f
Mathias <mathias.puyfages@gmail.com>
2025-01-11 23:51:36 +0100

    feat: add photo insertion.

--- resources/M_PHOTO.sql
+++ resources/M_PHOTO.sql
@@ -1,13 +1,11 @@
 CREATE TABLE photo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image LONGBLOB NOT NULL,
-    galery_id INT,
-    FOREIGN KEY (galery_id) REFERENCES galery(id)
-)
+id INT AUTO_INCREMENT PRIMARY KEY,
+image LONGBLOB NOT NULL,
+galery_id INT,
+FOREIGN KEY (galery_id) REFERENCES galery(id)
+);
 
-INSERT INTO galery (name) VALUES ('Nature Gallery'), ('Art Gallery'), ('Tech Gallery');
 
--- Insert test data into the photo table
 INSERT INTO photo (image, galery_id)
 VALUES
-    (LOAD_FILE('../images/test.jpeg'), 2),
+    (LOAD_FILE('./chat.jpeg'), 2)
