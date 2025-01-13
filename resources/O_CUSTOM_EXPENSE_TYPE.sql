47fe74299d2eb756ecfe650b51f2d1ecba595b37
Neptia <poomedyrungen@gmail.com>
2025-01-13 18:24:40 +0100

    feat: add app_expense_type and custom_expense_type

--- resources/O_CUSTOM_EXPENSE_TYPE.sql
+++ resources/O_CUSTOM_EXPENSE_TYPE.sql
@@ -4,16 +4,19 @@ CREATE TABLE custom_expense_type (
     budget INT NOT NULL,
     group_id INT NOT NULL,
     app_expense_type_id INT NOT NULL,
+    expense_id INT NOT NULL,
     FOREIGN KEY (group_id) REFERENCES `group`(id) ON DELETE CASCADE,
+    FOREIGN KEY (expense_id) REFERENCES `expense`(id) ON DELETE CASCADE,
     FOREIGN KEY (app_expense_type_id) REFERENCES `app_expense_type`(id) ON DELETE CASCADE
 );
 
-INSERT INTO custom_expense_type (label, budget, group_id, app_expense_type_id) VALUES
-('Tarbrew', 100, 1, 1),
-('Bus', 50, 1, 2),
-('Colloc', 200, 1, 3),
-('Patinage', 100, 1, 4),
-('KFC', 100, 2, 1),
-('Velo', 50, 2, 2),
-('Colloc 2', 200, 2, 3),
-('Cinema', 100, 2, 4);
+INSERT INTO custom_expense_type (label, budget, group_id, app_expense_type_id, expense_id) VALUES
+("Apple", 100, 1, 1, 1),
+("Bus", 50, 1, 2, 2),
+("Colloc", 200, 1, 3, 3),
+("Food", 100, 2, 1, 1),
+("Car", 50, 2, 2, 2),
+("Hotel", 200, 2, 3, 3),
+("Food", 100, 3, 1, 1),
+("Transport", 50, 3, 2, 2),
+("Accomodation", 200, 3, 3, 3);
