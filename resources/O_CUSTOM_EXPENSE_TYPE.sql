a91ddeb3144daa4eff43a20e86295971f2d4b8aa
Neptia <poomedyrungen@gmail.com>
2025-01-13 23:05:30 +0100

    fix: custom_expense_type expense is can be null

--- resources/O_CUSTOM_EXPENSE_TYPE.sql
+++ resources/O_CUSTOM_EXPENSE_TYPE.sql
@@ -4,7 +4,7 @@ CREATE TABLE custom_expense_type (
     budget INT NOT NULL,
     group_id INT,
     app_expense_type_id INT,
-    expense_id INT,
+    expense_id INT DEFAULT NULL,
     FOREIGN KEY (group_id) REFERENCES `group`(id) ON DELETE CASCADE,
     FOREIGN KEY (expense_id) REFERENCES `expense`(id) ON DELETE CASCADE,
     FOREIGN KEY (app_expense_type_id) REFERENCES `app_expense_type`(id) ON DELETE CASCADE
